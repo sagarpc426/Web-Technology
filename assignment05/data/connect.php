0b81794e8cc7ebe65abe3999c5f06e3361d8294d
sagarpc426 <51131279+sagarpc426@users.noreply.github.com>
2020-09-16 18:29:12 +0530

    Update connect.php

--- assignment05/data/connect.php
+++ assignment05/data/connect.php
@@ -3,7 +3,8 @@
 $conn = null;
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=trip", "root", "");
+    $conn = new PDO("mysql:host=localhost;dbname=id14770885_trip", "id14770885_spchaudhari", "Sagar@PWAK#1029");
+    // ithe server database cha host name lagel and ... root chay jage var username and "" madhe password 
 } catch(PDOException $e) {
     echo "Connection to data failed: " . $e->getMessage();
 }
